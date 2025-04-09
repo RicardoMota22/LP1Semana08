5a4a97bda876ae2bb0208b4f1452412e64cf3b44
RicardoMota22 <ricardomota.artist@gmail.com>
2025-04-09 22:19:16 +0100

    Add MilitaryUnit changes

--- GameUnit/MilitaryUnit.cs
+++ GameUnit/MilitaryUnit.cs
@@ -2,6 +2,10 @@ namespace GameUnit
 {
     public class MilitaryUnit : Unit // Heranca!!
     {
+        //public override int Health { get => base.Health; set => base.Health = value; }
+        public override int Health { get => base.Health + XP;}
+        public override float Cost { get => AttackPower + XP;}
+
         public int AttackPower { get; }
         public int XP { get; private set; }
 
@@ -9,6 +13,11 @@ namespace GameUnit
         {                                                        // Unit(int, int)
             AttackPower = attackPower;
             XP = 0;
+        }
+
+        public void Attack (Unit u)
+        {
+            XP = Health - AttackPower;
         }
     }
 }
