8ca2e9389bb4581dac98ea24842c45be30f2e6be
RicardoMota22 <ricardomota.artist@gmail.com>
2025-04-09 22:32:42 +0100

    Add Program changes

--- GameUnit/Program.cs
+++ GameUnit/Program.cs
@@ -6,7 +6,18 @@ namespace GameUnit
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            MilitaryUnit soldier = new MilitaryUnit(mov: 5,health: 100, attackPower: 20);
+            SettlerUnit worker = new SettlerUnit();
+
+            Console.WriteLine("Soldier:");
+            soldier.Move(3);
+
+            Console.WriteLine("Worker:");
+            worker.Move(2);
+
+            
+            Console.WriteLine($"Soldier Health: {soldier.Health}, Cost: {soldier.Cost}");
+            Console.WriteLine($"Worker Health: {worker.Health}, Cost: {worker.Cost}");
         }
     }
 }
