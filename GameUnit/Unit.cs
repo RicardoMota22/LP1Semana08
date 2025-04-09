55dcf95b1b2cf35f90587cb3917d99b1967b366e
RicardoMota22 <ricardomota.artist@gmail.com>
2025-04-09 22:07:23 +0100

    Add Unit changes

--- GameUnit/Unit.cs
+++ GameUnit/Unit.cs
@@ -1,14 +1,26 @@
+using System;
+using Humanizer;
+
 namespace GameUnit
 {
-    public class Unit
+    public abstract class Unit
     {
         private readonly int movement;
-        public int Health { get; set; }
+        public virtual int Health { get; set; }
+        //fica virtual para poder ser sobreposto nas classes derivadas
 
         public Unit(int movement, int health)
         {
             this.movement = movement;
             Health = health;
         }
+        public void Move(int spaces)
+        {
+            //Coverts int spaces to roman numerals
+            Console.WriteLine($"Unidade moveu {spaces.ToRoman()}");
+        }
+
+        public abstract float Cost{get;} // Propriedade read only
+        
     }
 }
